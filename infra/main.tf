f561013963c341a851d459612257477243abf711
Will Engler <Engler.Will@gmail.com>
2023-03-13 16:42:22 -0500

    add octet-stream binary content type to API

--- infra/main.tf
+++ infra/main.tf
@@ -100,7 +100,8 @@ resource "aws_cloudwatch_log_group" "garden_app" {
 /* API Gateway */
 
 resource "aws_api_gateway_rest_api" "garden_api" {
-  name          = "garden_gateway"
+  name               = "garden_gateway"
+  binary_media_types = ["application/octet-stream"]
 }
 
 resource "aws_api_gateway_stage" "garden_api" {
