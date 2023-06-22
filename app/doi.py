3e3fc22edce36485953bb8c7d0c0d507d0db864f
Isaac Darling <61167692+isaac-darling@users.noreply.github.com>
2023-06-22 11:20:28 -0500

    clean up logging messages

--- app/doi.py
+++ app/doi.py
@@ -12,10 +12,7 @@ logger = logging.getLogger()
 def call_datacite(event, _context, _kwargs):
     method = event["httpMethod"]
     payload = json.loads(event["body"])
-    print(str(payload))  # logger.info appears to not be high enough level to be seen
-    print(type(payload))
-    print(type(payload["data"]))
-    print(type(payload["data"]["attributes"]))
+    print(f"json {payload = }")  # logger.info appears to not be high enough level to be seen
     try:
         DATACITE_REPOSITORY_ID = os.environ["DATACITE_REPOSITORY_ID"]
         DATACITE_PASSWORD = os.environ["DATACITE_PASSWORD"]
