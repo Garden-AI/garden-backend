cc71f0d55405d76900dd62e0e1ae78f68467f190
isaac-darling <61167692+isaac-darling@users.noreply.github.com>
2023-07-03 12:01:05 -0500

    fix broken tests

--- app/test_app.py
+++ app/test_app.py
@@ -22,6 +22,8 @@ def test_doi(mocker) -> None:
             self.status_code = 200
         def json(self) -> dict:
             return {"data": {"attributes": {"doi": "10.23677/fake-doi"}}}
+        def raise_for_status(self) -> None:
+            pass
 
     mocker.patch("requests.post", return_value=DummyReply())
     mocker.patch("requests.put", return_value=DummyReply())
@@ -47,12 +49,12 @@ def test_presigned_url() -> None:
 
     assert (res:=lambda_handler(events[0], None))["statusCode"] == 400 and "direction" in json.loads(res["body"])["message"]
     assert (res:=lambda_handler(events[1], None))["statusCode"] == 400 and "format" in json.loads(res["body"])["message"]
-    assert (res:=lambda_handler(events[2], None))["statusCode"] == 500 and "access_key" in res["body"]
-    assert (res:=lambda_handler(events[3], None))["statusCode"] == 500 and "credentials" in res["body"]
+    assert (res:=lambda_handler(events[2], None))["statusCode"] == 200 and json.loads(res["body"])  # make sure return body is valid json
+    assert (res:=lambda_handler(events[3], None))["statusCode"] == 200 and json.loads(res["body"])
 
 
 def test_garden_search_record(mocker) -> None:
-    mocker.patch("globus_sdk.SearchClient.create_entry", return_value=None)
+    mocker.patch("globus_sdk.SearchClient.create_entry", return_value={"task_id": None})
     event = {"path": "/garden-search-record", "httpMethod": "POST", "body": json.dumps({"doi": "10.23677/fake-doi"})}
     mocker.patch("globus_sdk.SearchClient.get_task", return_value={"state": "PENDING", "task_id": "uuid-here", "fatal_error": "Globus error"})
     assert lambda_handler(event, None)["statusCode"] == 408
