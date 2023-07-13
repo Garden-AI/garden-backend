6418b810e45e3c7d2f98f30f223116d3b945a2f4
Isaac Darling <61167692+isaac-darling@users.noreply.github.com>
2023-07-13 09:32:28 -0500

    adjust one test and make another more strict

--- app/test_app.py
+++ app/test_app.py
@@ -49,9 +49,9 @@ def test_presigned_url() -> None:
     events = [{"path": "/presigned-url", "httpMethod": "POST", "body": payload} for payload in payloads]
 
     assert (res:=lambda_handler(events[0], None))["statusCode"] == 400 and "direction" in json.loads(res["body"])["message"]
-    assert (res:=lambda_handler(events[1], None))["statusCode"] == 400 and "format" in json.loads(res["body"])["responses"][0]["body"]
+    assert (res:=lambda_handler(events[1], None))["statusCode"] == 400 and "format" in json.loads(res["body"])["message"]
     assert (res:=lambda_handler(events[2], None))["statusCode"] == 200 and json.loads(res["body"])  # make sure return body is valid json
-    assert (res:=lambda_handler(events[3], None))["statusCode"] == 200 and json.loads(res["body"])
+    assert (res:=lambda_handler(events[3], None))["statusCode"] == 200 and (res:=json.loads(res["body"])["responses"])[0]["url"] and res[1]["url"]
 
 
 def test_garden_search_record(mocker) -> None:
