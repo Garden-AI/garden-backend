6e03723c6cef4b1d7e031b2cc492d16f9ea08e3b
Owen Price Skelly <21372141+OwenPriceSkelly@users.noreply.github.com>
2024-03-21 10:10:51 -0500

    make hello-world fastAPI app  (#66)
    * add source for hello-world fastAPI app
    
    * add a test
    
    * rename dir from app -> src
    
    * change image name in dev script

---
+++ garden-backend-service/run-dev-server.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+IMAGE_NAME="garden-service:dev"
+
+# Build the Docker image with dev dependencies
+echo "Building Docker image with development dependencies..."
+docker build --build-arg INSTALL_DEV=true -t $IMAGE_NAME .
+
+# Check if the build was successful
+if [ $? -eq 0 ]; then
+    echo "Successfully built the Docker image."
+else
+    echo "Failed to build the Docker image. Exiting..."
+    exit 1
+fi
+
+# Run like the deployment Docker container with the following tweaks:
+# - mount ./src as shared volume (instead of COPY) so local changes propagate into the container
+# - also mount ./tests so you can run pytest
+# - run uvicorn with --reload so those changes get picked up
+echo "Running the Docker container with live reload at http://localhost:5500 ..."
+docker run -p 5500:80 \
+    -v $(pwd)/src:/app/src \
+    -v $(pwd)/tests:/app/tests \
+    --name garden-service-dev-container \
+    --rm \
+    $IMAGE_NAME \
+    uvicorn src.main:app --host 0.0.0.0 --port 80 --reload
---
+++ garden-backend-service/tests/test_routes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from fastapi.testclient import TestClient
+
+from src.main import app
+
+client = TestClient(app)
+
+
+def test_greeting():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {
+        "Hello there": "You must be World. You seem familiar, have we met?"
+    }
