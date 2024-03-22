1e4c67f49622ede25d2d7b2cde1a141dccaf6cca
Owen Price Skelly <21372141+OwenPriceSkelly@users.noreply.github.com>
2024-03-22 14:04:25 -0500

    add outputs for lightsail module

---
+++ infra/modules/lightsail/outputs.tf
@@ -0,0 +1,19 @@
+output "container_service_name" {
+  description = "The name of the Lightsail container service."
+  value       = aws_lightsail_container_service.garden_service.id
+}
+
+output "container_service_arn" {
+  description = "The ARN of the Lightsail container service."
+  value       = aws_lightsail_container_service.garden_service.arn
+}
+
+output "container_service_state" {
+  description = "The current state of the service."
+  value       = aws_lightsail_container_service.garden_service.state
+}
+
+output "container_service_url" {
+  description = "The publicly accessible URL of the service."
+  value       = aws_lightsail_container_service.garden_service.url
+}
