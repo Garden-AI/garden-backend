cee5b0d7770f71c27ecd4a157e5e57f68b16f871
Owen Price Skelly <21372141+OwenPriceSkelly@users.noreply.github.com>
2024-03-25 10:16:46 -0500

    add tags

--- infra/modules/ecr/main.tf
+++ infra/modules/ecr/main.tf
@@ -1,6 +1,11 @@
 
 resource "aws_ecrpublic_repository" "ecr_repo" {
   repository_name = "garden-containers-${var.env}"
+  tags = {
+    Component      = "ECR"
+    Project        = "Garden"
+    RepositoryType = "Public"
+  }
 }
 
 resource "aws_iam_policy" "ecr_backend_write" {
@@ -11,8 +16,8 @@ resource "aws_iam_policy" "ecr_backend_write" {
     Version = "2012-10-17",
     Statement = [
       {
-        Effect    = "Allow",
-        Action    = [
+        Effect = "Allow",
+        Action = [
           "ecr-public:BatchCheckLayerAvailability",
           "ecr-public:PutImage",
           "ecr-public:InitiateLayerUpload",
@@ -22,15 +27,15 @@ resource "aws_iam_policy" "ecr_backend_write" {
         Resource = aws_ecrpublic_repository.ecr_repo.arn,
       },
       {
-        Effect    = "Allow",
-        Action    = [
+        Effect = "Allow",
+        Action = [
           "ecr-public:GetAuthorizationToken",
         ],
         Resource = "*"
       },
       {
-        Effect    = "Allow",
-        Action    = [
+        Effect = "Allow",
+        Action = [
           "sts:GetServiceBearerToken",
           "sts:AssumeRole"
         ],
--- infra/modules/lightsail/main.tf
+++ infra/modules/lightsail/main.tf
@@ -3,22 +3,32 @@ resource "aws_lightsail_container_service" "garden_service" {
   power       = "micro"
   scale       = 1
   is_disabled = false
+
+  tags = {
+    Component   = "Lightsail Container Service"
+    Project     = "Garden"
+    Environment = "${var.env}"
+  }
 }
 
 
 resource "aws_lightsail_container_service_deployment_version" "garden_service_deployment" {
-  container_service_name = aws_lightsail_container_service.garden_service.name
+  service_name = aws_lightsail_container_service.garden_service.name
   container {
-    image = "gardenai/garden-service:latest"
+    container_name = "garden-backend-service"
+    image          = "gardenai/garden-service:latest"
     ports = {
       80 = "HTTP"
+    }
+    environment = {
+      GARDEN_ENV = "${var.env}"
     }
   }
 
   public_endpoint {
     container_name = "garden-backend-service"
     container_port = 80
-    health_check   = {
+    health_check {
       healthy_threshold   = 2
       unhealthy_threshold = 2
       timeout_seconds     = 2
