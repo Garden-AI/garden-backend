a66825e8d03925a044eb517320abd43ca82c4122
Owen Price Skelly <21372141+OwenPriceSkelly@users.noreply.github.com>
2024-03-25 16:56:46 -0500

    move deployment config to aws-cli-readable json

--- infra/modules/lightsail/main.tf
+++ infra/modules/lightsail/main.tf
@@ -11,29 +11,3 @@ resource "aws_lightsail_container_service" "garden_service" {
   }
 }
 
-
-resource "aws_lightsail_container_service_deployment_version" "garden_service_deployment" {
-  service_name = aws_lightsail_container_service.garden_service.name
-  container {
-    container_name = "garden-backend-service"
-    image          = "gardenai/garden-service:latest"
-    ports = {
-      80 = "HTTP"
-    }
-    environment = {
-      GARDEN_ENV = "${var.env}"
-    }
-  }
-
-  public_endpoint {
-    container_name = "garden-backend-service"
-    container_port = 80
-    health_check {
-      healthy_threshold   = 2
-      unhealthy_threshold = 2
-      timeout_seconds     = 2
-      interval_seconds    = 5
-      path                = "/"
-    }
-  }
-}
