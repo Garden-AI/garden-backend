8b20bb18ac0588af82931f91df655ac19229fcf6
Owen Price Skelly <21372141+OwenPriceSkelly@users.noreply.github.com>
2024-04-02 09:03:01 -0500

    move greetings out of main

--- garden-backend-service/src/api/routes/greetings.py
+++ garden-backend-service/src/api/routes/greetings.py
@@ -1,7 +1,12 @@
 from fastapi import APIRouter, Depends
 from src.api.dependencies.auth import authenticated, AuthenticationState
 
-router = APIRouter()
+router = APIRouter(prefix="/greet")
+
+
+@router.get("/")
+async def greet_world(name: str):
+    return {"Hello there": "You must be World. You seem familiar, have we met?"}
 
 
 @router.get("/{name}")
@@ -9,7 +14,7 @@ async def greet_name(name: str):
     return {f"Hello, {name}": "Say hi to world for me."}
 
 
-@router.get("/")
+@router.get("/vip")
 async def greet_vip(auth: AuthenticationState = Depends(authenticated)):
     name = auth.username
     return {f"Welcome, {name}": "you're looking very... authentic this evening."}
--- garden-backend-service/src/main.py
+++ garden-backend-service/src/main.py
@@ -3,9 +3,4 @@ from src.api.routes import greetings
 
 app = FastAPI()
 
-app.include_router(greetings.router, prefix="/greetings")
-
-
-@app.get("/")
-def greet_world():
-    return {"Hello there": "You must be World. You seem familiar, have we met?"}
+app.include_router(greetings.router)
