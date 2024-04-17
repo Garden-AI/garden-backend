cc192243919f26c20c8b02615823dd981258d2c1
Owen Price Skelly <21372141+OwenPriceSkelly@users.noreply.github.com>
2024-04-17 12:02:01 -0500

    comments in schema module

--- garden-backend-service/src/api/schemas/datacite/__init__.py
+++ garden-backend-service/src/api/schemas/datacite/__init__.py
@@ -1,5 +1,17 @@
-from .full_schema import Doi as _Doi
-from .full_schema import Data3, DoiAttributes
+"""
+This module exposes a few useful pydantic models for dealing with the datacite
+rest api from the automatically generated `_full_schema` module. The
+automatically generated module was built from
+https://github.com/datacite/lupo/blob/master/openapi.yaml using the
+`datamodel-codegen` cli tool for pydantic.
+
+In some cases, the actual behavior of the datacite API might not correspond to
+the auto-generated schema. It's fine to tweak the auto-generated models in those cases,
+otherwise if we want to change the behavior of some sub-field prefer inheriting with our own
+pydantic class (like below) .
+"""
+from ._full_schema import Doi as _Doi
+from ._full_schema import Data3, DoiAttributes
 
 
 class DoiData(Data3):
@@ -9,6 +21,7 @@ class DoiData(Data3):
 
 
 class Doi(_Doi):
+    # make data field non-optional
     data: DoiData
 
 
