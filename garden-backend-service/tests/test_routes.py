fc89b32c7359019a46f7d1e8d6d1a3ebe32e5713
Owen Price Skelly <21372141+OwenPriceSkelly@users.noreply.github.com>
2024-04-18 16:58:22 -0500

    unit test

--- garden-backend-service/tests/test_routes.py
+++ garden-backend-service/tests/test_routes.py
@@ -1,5 +1,6 @@
 import pytest
 import requests
+import json
 from fastapi.testclient import TestClient
 from unittest.mock import MagicMock, patch
 
@@ -21,12 +22,21 @@ def mock_auth_state():
 
 @pytest.fixture(autouse=True)
 def mock_boto3_get_secret_value():
+    # dummy values for config read from secrets
+    MOCK_SECRETS_CONFIG_SETTINGS = {
+        "DATACITE_PREFIX": "PREFIX",
+        "DATACITE_ENDPOINT": "ENDPOINT",
+        "DATACITE_REPO_ID": "REPO_ID",
+        "DATACITE_PASSWORD": "PASSWORD",
+        "ECR_REPO_ARN": "ECR_REPO_ARN",
+        "ECR_ROLE_ARN": "ECR_ROLE_ARN",
+    }
     # Typical secret response format
     mock_secret_value_response = {
         "ARN": "arn:aws:secretsmanager:us-east-1:1234567890:secret:test",
         "Name": "test",
         "VersionId": "a1b2c3d4-5678-90ab-cdef-EXAMPLE11111",
-        "SecretString": '{"DATACITE_PREFIX": "PREFIX", "DATACITE_ENDPOINT": "ENDPOINT", "DATACITE_REPO_ID": "REPO_ID", "DATACITE_PASSWORD": "PASSWORD"}',
+        "SecretString": json.dumps(MOCK_SECRETS_CONFIG_SETTINGS),
         "VersionStages": ["AWSCURRENT"],
         "CreatedDate": "2021-01-22T21:46:32.725000+08:00",
     }
@@ -43,7 +53,15 @@ def mock_settings():
     mock_settings.DATACITE_ENDPOINT = "http://localhost:8000"
     mock_settings.DATACITE_REPO_ID = "REPO_ID"
     mock_settings.DATACITE_PASSWORD = "PASSWORD"
+    mock_settings.ECR_REPO_ARN = "ECR_REPO_ARN"
+    mock_settings.ECR_ROLE_ARN = "ECR_ROLE_ARN"
+    mock_settings.STS_TOKEN_TIMEOUT = 0
     return mock_settings
+
+
+@pytest.fixture(autouse=True)
+def mock_get_settings(monkeypatch, mock_settings):
+    monkeypatch.setattr("src.config.get_settings", lambda: mock_settings)
 
 
 @pytest.fixture
@@ -127,3 +145,25 @@ def test_update_datacite(
 
     assert response.status_code == 200
     assert response.json() == mock_request_body
+
+
+def test_get_push_session(
+    override_get_settings_dependency, override_authenticated_dependency
+):
+    mock_assume_role = {
+        "Credentials": {
+            "AccessKeyId": "testAccessKey",
+            "SecretAccessKey": "testSecretKey",
+            "SessionToken": "testSessionToken",
+        }
+    }
+
+    with patch("boto3.client") as mock_client:
+        client_instance = mock_client.return_value
+        client_instance.assume_role.return_value = mock_assume_role
+
+        response = client.get("/docker-push-token/")
+
+    assert response.status_code == 200
+    for key in ["AccessKeyId", "SecretAccessKey", "SessionToken", "ECRRepo"]:
+        assert key in response.json()
