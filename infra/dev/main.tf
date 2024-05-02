8b531d6290b03fc2d360db0e155f8ad91c233641
Owen Price Skelly <21372141+OwenPriceSkelly@users.noreply.github.com>
2024-05-02 12:30:31 -0500

    misc resources for api-dev.thegardens.ai subdomain

--- infra/dev/main.tf
+++ infra/dev/main.tf
@@ -62,9 +62,33 @@ module "ecr" {
 }
 
 module "lightsail" {
-  source                = "../modules/lightsail"
-  aws_account_id        = var.aws_account_id
-  env                   = var.env
-  ecr_access_policy_arn = module.ecr.ecr_backend_write_policy_arn
-  s3_access_policy_arn  = module.s3.full_access_arn
+  source                            = "../modules/lightsail"
+  aws_account_id                    = var.aws_account_id
+  env                               = var.env
+  ecr_access_policy_arn             = module.ecr.ecr_backend_write_policy_arn
+  s3_access_policy_arn              = module.s3.full_access_arn
+  lightsail_certificate_name        = "api-dev-certificate"
+  lightsail_certificate_domain_name = data.aws_acm_certificate.api_cert.domain
+}
+
+/* point api-dev.thegardens.ai to the lightsail deployment */
+
+data "aws_route53_zone" "hosted_zone" {
+  name = "thegardens.ai"
+}
+
+data "aws_acm_certificate" "api_cert" {
+  domain = "api-dev.thegardens.ai"
+}
+
+resource "aws_route53_record" "api_record" {
+  name    = data.aws_acm_certificate.api_cert.domain
+  type    = "A"
+  zone_id = data.aws_route53_zone.hosted_zone.id
+
+  alias {
+    evaluate_target_health = true
+    name                   = module.lightsail.container_service_domain
+    zone_id                = module.lightsail.lightsail_container_service_hosted_zone_id
+  }
 }
