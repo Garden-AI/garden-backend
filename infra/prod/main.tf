bc340d10b8ee552d878bbd35781e317f532276cd
Owen Price Skelly <21372141+OwenPriceSkelly@users.noreply.github.com>
2024-05-02 12:37:15 -0500

    analogous changes to prod for api.thegardens.ai

--- infra/prod/main.tf
+++ infra/prod/main.tf
@@ -62,14 +62,16 @@ module "ecr" {
 }
 
 module "lightsail" {
-  source                = "../modules/lightsail"
-  aws_account_id        = var.aws_account_id
-  env                   = var.env
-  ecr_access_policy_arn = module.ecr.ecr_backend_write_policy_arn
-  s3_access_policy_arn  = module.s3.full_access_arn
+  source                            = "../modules/lightsail"
+  aws_account_id                    = var.aws_account_id
+  env                               = var.env
+  ecr_access_policy_arn             = module.ecr.ecr_backend_write_policy_arn
+  s3_access_policy_arn              = module.s3.full_access_arn
+  lightsail_certificate_name        = "api-certificate"
+  lightsail_certificate_domain_name = data.aws_acm_certificate.api_cert.domain
 }
 
-/* (prod only) connect api.thegardens.ai to the gateway */
+/* point api.thegardens.ai to the lightsail deployment */
 
 data "aws_route53_zone" "hosted_zone" {
   name = "thegardens.ai"
@@ -79,20 +81,14 @@ data "aws_acm_certificate" "api_cert" {
   domain = "api.thegardens.ai"
 }
 
-resource "aws_api_gateway_domain_name" "api_domain_name" {
-  certificate_arn = data.aws_acm_certificate.api_cert.arn
-  domain_name     = data.aws_acm_certificate.api_cert.domain
-}
-
-
 resource "aws_route53_record" "api_record" {
-  name    = aws_api_gateway_domain_name.api_domain_name.domain_name
+  name    = data.aws_acm_certificate.api_cert.domain
   type    = "A"
   zone_id = data.aws_route53_zone.hosted_zone.id
 
   alias {
     evaluate_target_health = true
-    name                   = aws_api_gateway_domain_name.api_domain_name.cloudfront_domain_name
-    zone_id                = aws_api_gateway_domain_name.api_domain_name.cloudfront_zone_id
+    name                   = module.lightsail.container_service_domain
+    zone_id                = module.lightsail.lightsail_container_service_hosted_zone_id
   }
 }
