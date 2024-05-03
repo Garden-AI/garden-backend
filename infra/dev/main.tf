d2d32b859f23d7eeff6d3235cfddfd4144ed5a34
Owen Price Skelly <21372141+OwenPriceSkelly@users.noreply.github.com>
2024-05-03 12:23:24 -0500

    remove api gateway/lambda references from main.tf

--- infra/dev/main.tf
+++ infra/dev/main.tf
@@ -26,27 +26,7 @@ module "secrets_manager" {
 
   env                     = var.env
   aws_account_id          = var.aws_account_id
-  lambda_exec_role_name   = module.lambda.lambda_exec_role_name
   lightsail_iam_user_name = module.lightsail.lightsail_iam_user_name
-}
-
-module "api_gateway" {
-  source = "../modules/api_gateway"
-
-  env                             = var.env
-  garden_app_invoke_arn           = module.lambda.garden_app_invoke_arn
-  garden_app_function_name        = module.lambda.garden_app_function_name
-  garden_authorizer_invoke_arn    = module.lambda.garden_authorizer_invoke_arn
-  garden_authorizer_function_name = module.lambda.garden_authorizer_function_name
-}
-
-module "lambda" {
-  source = "../modules/lambda"
-
-  env                       = var.env
-  api_gateway_execution_arn = module.api_gateway.api_execution_arn
-  s3_access_policy_arn      = module.s3.full_access_arn
-  ecr_access_policy_arn     = module.ecr.ecr_backend_write_policy_arn
 }
 
 module "s3" {
--- infra/dev/outputs.tf
+++ infra/dev/outputs.tf
@@ -1,15 +1,9 @@
 # Output value definitions
 
-output "function_name" {
-  description = "Name of the Lambda function."
-
-  value = module.lambda.garden_app_function_name
-}
-
 output "base_url" {
-  description = "Base URL for API Gateway stage."
+  description = "Base URL for lightsail deployment."
 
-  value = module.api_gateway.stage_invoke_url
+  value = module.lightsail.container_service_domain
 }
 
 output "container_service_name" {
@@ -25,4 +19,9 @@ output "container_service_state" {
 output "container_service_url" {
   description = "The publicly accessible URL of the service."
   value       = module.lightsail.container_service_url
+}
+
+output "api_endpoint_url" {
+  description = "The full URL for the API endpoint"
+  value = "https://${var.subdomain_prefix}.${var.root_domain_name}"
 }
