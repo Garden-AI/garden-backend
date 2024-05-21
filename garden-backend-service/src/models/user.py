fe5e372535f483959009d87546def9e298bdfcb4
Owen Price Skelly <21372141+OwenPriceSkelly@users.noreply.github.com>
2024-05-21 16:12:50 -0500

    add sqlalchemy models

---
+++ garden-backend-service/src/models/user.py
@@ -0,0 +1,12 @@
+from uuid import UUID
+
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str]
+    identity_id: Mapped[UUID] = mapped_column(unique=True)
