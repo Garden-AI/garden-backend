08c2d2807b33e8a973b3a7c08cc7c2832c600d92
Owen Price Skelly <21372141+OwenPriceSkelly@users.noreply.github.com>
2024-05-21 16:14:55 -0500

    add `authed_user` dependency; modify /greet to use

--- garden-backend-service/src/api/routes/greet.py
+++ garden-backend-service/src/api/routes/greet.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, Depends
-from src.api.dependencies.auth import authenticated, AuthenticationState
+from src.api.dependencies.auth import authed_user
+from src.models.user import User
 
 router = APIRouter(prefix="/greet")
 
 
 @router.get("")
-async def greet_authed_user(auth: AuthenticationState = Depends(authenticated)):
-    name = auth.username
+async def greet_authed_user(user: User = Depends(authed_user)):
+    name = await user.awaitable_attrs.username
     return {f"Welcome, {name}": "you're looking very... authentic today."}
