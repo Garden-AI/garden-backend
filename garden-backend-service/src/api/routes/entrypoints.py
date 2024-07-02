7f89ca9565b9fb2db47ebc2a71f937d990978698
Owen Price Skelly <21372141+OwenPriceSkelly@users.noreply.github.com>
2024-07-02 11:00:37 -0500

    demote related metadata to JSON

--- garden-backend-service/src/api/routes/entrypoints.py
+++ garden-backend-service/src/api/routes/entrypoints.py
@@ -3,7 +3,6 @@ from logging import getLogger
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import QueryableAttribute, RelationshipProperty
 from src.api.dependencies.auth import authed_user
 from src.api.dependencies.database import get_db_session
 from src.api.routes._utils import assert_deletable_by_user
@@ -11,7 +10,7 @@ from src.api.schemas.entrypoint import (
     EntrypointCreateRequest,
     EntrypointMetadataResponse,
 )
-from src.models import Base, Entrypoint, User
+from src.models import Entrypoint, User
 
 logger = getLogger(__name__)
 
@@ -24,7 +23,34 @@ async def add_entrypoint(
     db: AsyncSession = Depends(get_db_session),
     user: User = Depends(authed_user),
 ):
-    return await _create_new_entrypoint(entrypoint, db, user)
+    # default owner is authed_user unless owner_identity_id is explicitly provided
+    owner: User = user
+    if entrypoint.owner_identity_id is not None:
+        explicit_owner: User | None = await User.get(
+            db, identity_id=entrypoint.owner_identity_id
+        )
+        if explicit_owner is not None:
+            owner = explicit_owner
+        else:
+            logger.warning(
+                f"No user found with Globus identity ID {entrypoint.owner_identity_id}. "
+                f"Assigning default ownership to {user.identity_id} ({user.username}). "
+            )
+
+    new_entrypoint = Entrypoint.from_dict(
+        entrypoint.model_dump(exclude="owner_identity_id")
+    )
+    new_entrypoint.owner = owner
+    db.add(new_entrypoint)
+    try:
+        await db.commit()
+    except IntegrityError as e:
+        await db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Entrypoint with this DOI already exists",
+        ) from e
+    return new_entrypoint
 
 
 # note: the ':path' is a starlette converter option to include '/' characters
@@ -72,91 +98,3 @@ async def delete_entrypoint(
     else:
         # no error if not found so DELETE is idempotent
         return {"detail": f"No entrypoint found with DOI {doi}."}
-
-
-@router.put("/{doi:path}", response_model=EntrypointMetadataResponse)
-async def put_entrypoint_by_doi(
-    doi: str,
-    entrypoint: EntrypointCreateRequest,
-    db: AsyncSession = Depends(get_db_session),
-    user: User = Depends(authed_user),
-):
-    if entrypoint.doi != doi:
-        raise HTTPException(
-            status=status.HTTP_400_BAD_REQUEST,
-            detail="payload DOI does not match request URL",
-        )
-
-    existing_entrypoint: Entrypoint | None = await Entrypoint.get(db, doi=doi)
-    if existing_entrypoint is None:
-        return await _create_new_entrypoint(entrypoint, db, user)
-
-    if existing_entrypoint.owner_id != user.id:
-        raise HTTPException(
-            status_code=status.HTTP_403_FORBIDDEN,
-            detail="You do not have permission to modify this entrypoint.",
-        )
-
-    # current owner can change the owner ID to someone else
-    if entrypoint.owner_identity_id:
-        new_owner: User | None = await User.get(
-            db, identity_id=entrypoint.owner_identity_id
-        )
-        if new_owner:
-            existing_entrypoint.owner = new_owner
-
-    # update with values from payload, creating new objects for papers, datasets etc.
-    for key, value in entrypoint.model_dump(exclude="owner_identity_id").items():
-        orm_attribute: QueryableAttribute = getattr(Entrypoint, key)
-        is_relationship = isinstance(orm_attribute.property, RelationshipProperty)
-        # handle one-many relationships (i.e. the related metadata types)
-        if isinstance(value, list) and is_relationship:
-            related_cls: Base = orm_attribute.property.mapper.class_
-            new_items: list[Base] = [related_cls.from_dict(item) for item in value]
-            setattr(existing_entrypoint, key, new_items)
-        else:
-            setattr(existing_entrypoint, key, value)
-
-    try:
-        await db.commit()
-    except IntegrityError as e:
-        await db.rollback()
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=f"Failed to update entrypoint with doi {doi}",
-        ) from e
-
-
-async def _create_new_entrypoint(
-    entrypoint: EntrypointCreateRequest,
-    db: AsyncSession,
-    user: User,
-):
-    # default owner is authed_user unless owner_identity_id is explicitly provided
-    owner: User = user
-    if entrypoint.owner_identity_id is not None:
-        explicit_owner: User | None = await User.get(
-            db, identity_id=entrypoint.owner_identity_id
-        )
-        if explicit_owner is not None:
-            owner = explicit_owner
-        else:
-            logger.warning(
-                f"No user found with Globus identity ID {entrypoint.owner_identity_id}. "
-                f"Assigning default ownership to {user.identity_id} ({user.username}). "
-            )
-
-    new_entrypoint = Entrypoint.from_dict(
-        entrypoint.model_dump(exclude="owner_identity_id")
-    )
-    new_entrypoint.owner = owner
-    db.add(new_entrypoint)
-    try:
-        await db.commit()
-    except IntegrityError as e:
-        await db.rollback()
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Entrypoint with this DOI already exists",
-        ) from e
-    return new_entrypoint
--- garden-backend-service/src/models/entrypoint.py
+++ garden-backend-service/src/models/entrypoint.py
@@ -2,18 +2,12 @@ from typing import TYPE_CHECKING
 from uuid import UUID
 
 from sqlalchemy import ForeignKey, String
-from sqlalchemy.dialects import postgresql
+from sqlalchemy.dialects.postgresql import ARRAY, JSON
 from sqlalchemy.orm import Mapped, mapped_column, relationship, synonym
 from src.models.base import Base
 
 if TYPE_CHECKING:
     from src.models.garden import Garden
-    from src.models.related_metadata import (
-        DatasetMetadata,
-        ModelMetadata,
-        PaperMetadata,
-        RepositoryMetadata,
-    )
     from src.models.user import User
 
 else:
@@ -42,34 +36,17 @@ class Entrypoint(Base):
     short_name: Mapped[str]
     function_text: Mapped[str]
 
-    authors: Mapped[list[str]] = mapped_column(postgresql.ARRAY(String))
-    tags: Mapped[list[str]] = mapped_column(postgresql.ARRAY(String))
-    test_functions: Mapped[list[str]] = mapped_column(postgresql.ARRAY(String))
-    requirements: Mapped[list[str]] = mapped_column(postgresql.ARRAY(String))
+    authors: Mapped[list[str]] = mapped_column(ARRAY(String))
+    tags: Mapped[list[str]] = mapped_column(ARRAY(String))
+    test_functions: Mapped[list[str]] = mapped_column(ARRAY(String))
+    requirements: Mapped[list[str] | None] = mapped_column(ARRAY(String))
 
-    # lazy=selectin to load eagerly by default; cascade options are set to
-    # everything but "refresh-expire" for asyncio reasons.
-    # (see: https://docs.sqlalchemy.org/en/20/orm/cascades.html)
-    models: Mapped[list[ModelMetadata]] = relationship(
-        back_populates="entrypoint",
-        lazy="selectin",
-        cascade="save-update, merge, expunge, delete, delete-orphan",
-    )
-    repositories: Mapped[list[RepositoryMetadata]] = relationship(
-        back_populates="entrypoint",
-        lazy="selectin",
-        cascade="save-update, merge, expunge, delete, delete-orphan",
-    )
-    papers: Mapped[list[PaperMetadata]] = relationship(
-        back_populates="entrypoint",
-        lazy="selectin",
-        cascade="save-update, merge, expunge, delete, delete-orphan",
-    )
-    datasets: Mapped[list[DatasetMetadata]] = relationship(
-        back_populates="entrypoint",
-        lazy="selectin",
-        cascade="save-update, merge, expunge, delete, delete-orphan",
-    )
+    # NOTE: modifications to these lists / dictionaries won't be picked up
+    # by sqlalchemy. Replace the attribute with an updated copy instead
+    models: Mapped[list[dict] | None] = mapped_column(ARRAY(JSON))
+    repositories: Mapped[list[dict] | None] = mapped_column(ARRAY(JSON))
+    papers: Mapped[list[dict] | None] = mapped_column(ARRAY(JSON))
+    datasets: Mapped[list[dict] | None] = mapped_column(ARRAY(JSON))
 
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
     user: Mapped[User] = relationship(lazy="selectin")
