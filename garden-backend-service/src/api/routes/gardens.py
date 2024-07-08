1d2b282747db4542fa4e4dc89bf8dfb252db4462
hholb <hholbroo@unca.edu>
2024-07-08 08:41:42 -0400

    Add search_gardens GET route that allows for more advanced queries
    Added unit tests to verify the functionality

--- garden-backend-service/src/api/routes/gardens.py
+++ garden-backend-service/src/api/routes/gardens.py
@@ -1,6 +1,8 @@
 from logging import getLogger
+from typing import Optional
+from uuid import UUID
 
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, Query, status
 from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -21,6 +23,62 @@ async def add_garden(
     user: User = Depends(authed_user),
 ):
     return await _create_new_garden(garden, db, user)
+
+
+@router.get("", response_model=list[GardenMetadataResponse])
+async def search_gardens(
+    uuid: Optional[UUID] = Query(None, description="Filter by user UUID"),
+    authors: Optional[str] = Query(None, description="Comma-separated list of authors"),
+    contributors: Optional[str] = Query(
+        None, description="Comma-separated list of contributors"
+    ),
+    tags: Optional[str] = Query(None, description="Comma-separated list of tags"),
+    year: Optional[str] = Query(None, description="Filter by year"),
+    limit: int = Query(
+        20, description="Limit number of gardens returned by the query."
+    ),
+    db: AsyncSession = Depends(get_db_session),
+):
+    """
+    Return a list of gardens.
+
+    With no query params, this returns the default limit number of gardens.
+    Add limit=0 to get all gardens.
+
+    Query params for filtering by:
+     - user_id
+     - authors
+     - contributors
+     - tags
+     - year
+    """
+    stmt = select(Garden)
+
+    if uuid is not None:
+        stmt = stmt.join(Garden.user).where(User.identity_id == uuid)
+
+    if authors:
+        authors_list = authors.split(",")
+        stmt = stmt.where(Garden.authors.contains(authors_list))
+
+    if contributors:
+        contributors_list = contributors.split(",")
+        stmt = stmt.where(Garden.contributors.contains(contributors_list))
+
+    if tags:
+        tags_list = tags.split(",")
+        stmt = stmt.where(Garden.tags.contains(tags_list))
+
+    if year is not None:
+        stmt = stmt.where(Garden.year == year)
+
+    if limit > 0:
+        stmt = stmt.limit(limit)
+
+    result = await db.execute(stmt)
+    gardens = result.scalars().all()
+
+    return gardens
 
 
 @router.get(
