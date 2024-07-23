2b407a4cf9467d05ef25aea678c50246160bd8b0
Hayden Holbrook <111379706+hholb@users.noreply.github.com>
2024-07-23 11:54:31 -0400

    Create routes for user's saved gardens (#121)
    * Create GET and PATCH routes for user's saved gardens
    
    * Add delete route
    
    * Remove uneeeded exception
    
    * Style changes
    
    * Change route signature, use orm style udpates, +tests
    
    * update doc comments
    
    * bump gunicorn to 22.0.0
    
    * poetry lock
    
    * Make routes idempotent

--- garden-backend-service/src/api/routes/users.py
+++ garden-backend-service/src/api/routes/users.py
@@ -1,10 +1,13 @@
 from typing import Any
+from uuid import UUID
 
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy import select
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.api.dependencies.auth import authed_user
 from src.api.dependencies.database import get_db_session
+from src.api.schemas.garden import GardenMetadataResponse
 from src.api.schemas.user import UserMetadataResponse, UserUpdateRequest
 from src.models import Garden, User
 from src.models._associations import users_saved_gardens
@@ -58,3 +61,118 @@ async def update_user_info(
         saved_garden_dois=saved_garden_dois,
     )
     return user_response
+
+
+@router.get("/{user_uuid}/saved/gardens", response_model=list[GardenMetadataResponse])
+async def get_saved_gardens(
+    user_uuid: UUID,
+    db: AsyncSession = Depends(get_db_session),
+) -> list[GardenMetadataResponse]:
+    """Fetch the users list of saved gardens."""
+    user: User | None = await User.get(db, identity_id=user_uuid)
+
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"No user with uuid {user_uuid} found.",
+        )
+
+    # We need to explicitly load the saved_gardens field
+    await db.refresh(user, ["saved_gardens"])
+    return [
+        GardenMetadataResponse.model_validate(garden) for garden in user.saved_gardens
+    ]
+
+
+@router.put(
+    "/{user_uuid}/saved/gardens/{doi:path}", response_model=list[GardenMetadataResponse]
+)
+async def save_garden(
+    user_uuid: UUID,
+    doi: str,
+    authed_user: User = Depends(authed_user),
+    db: AsyncSession = Depends(get_db_session),
+) -> list[GardenMetadataResponse]:
+    """Add a garden to the user's list of saved gardens by doi."""
+    user: User | None = await User.get(db, identity_id=user_uuid)
+    garden: Garden | None = await Garden.get(db, doi=doi)
+
+    if user_uuid != authed_user.identity_id:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"Not authorized to save a garden for user {user_uuid}.",
+        )
+
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"No user with user_uuid {user_uuid} found.",
+        )
+
+    if garden is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"No garden with doi {doi} found.",
+        )
+
+    await db.refresh(user, ["saved_gardens"])
+    if garden not in user.saved_gardens:
+        user.saved_gardens.append(garden)
+
+    try:
+        await db.commit()
+        return user.saved_gardens
+    except IntegrityError as e:
+        await db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="User has already saved this garden.",
+        ) from e
+
+
+@router.delete("/{user_uuid}/saved/gardens/{doi:path}")
+async def remove_saved_garden(
+    user_uuid: UUID,
+    doi: str,
+    authed_user: User = Depends(authed_user),
+    db: AsyncSession = Depends(get_db_session),
+) -> list[GardenMetadataResponse]:
+    """Remove a garden from the user's list of saved gardens by doi."""
+
+    if user_uuid != authed_user.identity_id:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"Not authorized to remove a saved garden for user {user_uuid}.",
+        )
+
+    user: User | None = await User.get(db, identity_id=user_uuid)
+    garden: Garden | None = await Garden.get(db, doi=doi)
+
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"No user with user_uuid {user_uuid} found.",
+        )
+
+    if garden is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"No garden with doi {doi} found.",
+        )
+
+    await db.refresh(user, ["saved_gardens"])
+    if garden in user.saved_gardens:
+        user.saved_gardens.remove(garden)
+
+    try:
+        await db.commit()
+        return [
+            GardenMetadataResponse.model_validate(garden)
+            for garden in user.saved_gardens
+        ]
+    except IntegrityError as e:
+        await db.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=f"Unable to remove saved garden with doi {doi} due to integrity contraints",
+        ) from e
--- garden-backend-service/tests/api/routes/test_users.py
+++ garden-backend-service/tests/api/routes/test_users.py
@@ -115,3 +115,149 @@ async def test_unauthorized_access(
     response = await client.patch("/users", json=update_payload)
     assert response.status_code == 403
     assert response.json()["detail"] == "Authorization header missing"
+
+
+@pytest.mark.asyncio
+@pytest.mark.integration
+async def test_save_garden(
+    client,
+    mock_db_session,
+    override_authenticated_dependency,
+    mock_auth_state,
+    mock_garden_create_request_no_entrypoints_json,
+):
+    # Post the garden we will save
+    doi = mock_garden_create_request_no_entrypoints_json["doi"]
+    res = await client.post(
+        "/gardens", json=mock_garden_create_request_no_entrypoints_json
+    )
+    assert res.status_code == 200
+
+    # Post some other gardens
+    for i in range(5):
+        mock_garden_create_request_no_entrypoints_json["doi"] = f"fake/doi-{i}"
+        res = await client.post(
+            "/gardens", json=mock_garden_create_request_no_entrypoints_json
+        )
+        assert res.status_code == 200
+
+    # Save the garden, we should get the updated list of saved gardens back
+    res = await client.put(f"/users/{mock_auth_state.identity_id}/saved/gardens/{doi}")
+    assert res.status_code == 200
+    data = res.json()
+    assert len(data) == 1
+    assert doi == data[0]["doi"]
+
+    # Saving the garden again should be idempotent
+    res = await client.put(f"/users/{mock_auth_state.identity_id}/saved/gardens/{doi}")
+    assert res.status_code == 200
+
+    # Get the users saved gardens, should only have 1
+    res = await client.get(f"/users/{mock_auth_state.identity_id}/saved/gardens")
+    assert res.status_code == 200
+    data = res.json()
+    assert len(data) == 1
+    assert doi == data[0]["doi"]
+
+    # Remove the saved garden
+    res = await client.delete(
+        f"/users/{mock_auth_state.identity_id}/saved/gardens/{doi}"
+    )
+    assert res.status_code == 200
+    data = res.json()
+    assert len(data) == 0
+
+    # Removing the same saved garden again should be idempotent
+    res = await client.delete(
+        f"/users/{mock_auth_state.identity_id}/saved/gardens/{doi}"
+    )
+    assert res.status_code == 200
+
+    # Get the users saved gardens again, make sure the garden is not present
+    res = await client.get(f"/users/{mock_auth_state.identity_id}/saved/gardens")
+    assert res.status_code == 200
+    data = res.json()
+    assert len(data) == 0
+
+
+@pytest.mark.asyncio
+@pytest.mark.integration
+async def test_save_garden_no_auth(
+    client,
+    mock_missing_token,
+    mock_db_session,
+):
+    response = await client.put("/users/some_UUID/saved/gardens/somedoi")
+    assert response.status_code == 403
+
+    response = await client.delete("users/some_UUID/saved/gardens/somedoi")
+    assert response.status_code == 403
+
+
+@pytest.mark.asyncio
+@pytest.mark.integration
+async def test_save_garden_unauthorized(
+    client,
+    mock_db_session,
+    mock_auth_state,
+    mock_auth_state_other_user,
+    mock_garden_create_request_no_entrypoints_json,
+    override_authenticated_dependency,
+):
+    # Post a garden
+    doi = mock_garden_create_request_no_entrypoints_json["doi"]
+    res = await client.post(
+        "/gardens", json=mock_garden_create_request_no_entrypoints_json
+    )
+    assert res.status_code == 200
+
+    # Try to save it to another users list of saved gardens
+    result = await client.put(
+        f"/users/{mock_auth_state_other_user.identity_id}/saved/gardens/{doi}"
+    )
+    assert result.status_code == 401
+
+
+@pytest.mark.asyncio
+@pytest.mark.integration
+async def test_delete_saved_garden_unauthorized(
+    client,
+    mock_db_session,
+    mock_auth_state,
+    mock_auth_state_other_user,
+    mock_garden_create_request_no_entrypoints_json,
+    override_authenticated_dependency,
+):
+    # Post a garden
+    doi = mock_garden_create_request_no_entrypoints_json["doi"]
+    res = await client.post(
+        "/gardens", json=mock_garden_create_request_no_entrypoints_json
+    )
+    assert res.status_code == 200
+
+    # Try and delete another users list of saved gardens
+    result = await client.delete(
+        f"/users/{mock_auth_state_other_user.identity_id}/saved/gardens/{doi}"
+    )
+    assert result.status_code == 401
+
+
+@pytest.mark.asyncio
+@pytest.mark.integration
+async def test_save_garden_nonexistent_garden(
+    client,
+    mock_db_session,
+    mock_auth_state,
+    override_authenticated_dependency,
+):
+    # Try and save a garden that doesn't exist
+    result = await client.put(
+        f"/users/{mock_auth_state.identity_id}/saved/gardens/somedoi"
+    )
+    assert result.status_code == 404
+
+    # Try and remove a saved garden that doesn't exist
+    result = await client.delete(
+        f"/users/{mock_auth_state.identity_id}/saved/gardens/somedoi"
+    )
+    assert result.status_code == 404
