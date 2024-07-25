4fe38a12f9120c4f9c01a6556aa4ecdc87daaab9
MaxTuecke <maxtuecke@gmail.com>
2024-07-25 11:44:28 -0500

    update alembic migrations

--- garden-backend-service/migrations/versions/92f6722af9e5_create_mdf_datasets_table.py
+++ garden-backend-service/migrations/versions/92f6722af9e5_create_mdf_datasets_table.py
@@ -1,8 +1,8 @@
 """create mdf datasets table
 
-Revision ID: 3420eb0c0c7d
+Revision ID: 92f6722af9e5
 Revises: f293268b31c5
-Create Date: 2024-07-24 21:28:16.362804
+Create Date: 2024-07-25 16:43:06.732761
 
 """
 
@@ -13,7 +13,7 @@ from alembic import op
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
-revision: str = "3420eb0c0c7d"
+revision: str = "92f6722af9e5"
 down_revision: Union[str, None] = "f293268b31c5"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -29,14 +29,15 @@ def upgrade() -> None:
         sa.Column("source_name", sa.String(), nullable=False),
         sa.Column("version", sa.String(), nullable=False),
         sa.Column("user_id", sa.Integer(), nullable=False),
-        sa.Column("flow_action_id", sa.String(), nullable=False),
-        sa.Column("previous_versions", postgresql.ARRAY(sa.String()), nullable=False),
+        sa.Column("flow_action_id", sa.Uuid(), nullable=False),
+        sa.Column("previous_versions", postgresql.ARRAY(sa.String()), nullable=True),
         sa.ForeignKeyConstraint(
             ["user_id"],
             ["users.id"],
         ),
         sa.PrimaryKeyConstraint("id"),
         sa.UniqueConstraint("doi"),
+        sa.UniqueConstraint("flow_action_id"),
         sa.UniqueConstraint("versioned_source_id"),
     )
     # ### end Alembic commands ###
