4d965442978db304c33d2d0878784f300cb41038
Will Engler <Engler.Will@gmail.com>
2024-09-25 15:44:11 -0500

    lookup Modal function so app is warm

--- garden-backend-service/src/api/dependencies/sandboxed_functions.py
+++ garden-backend-service/src/api/dependencies/sandboxed_functions.py
@@ -3,22 +3,38 @@ from src.config import get_settings, Settings
 from src.sandboxed_functions.modal_publishing_helpers import (
     validate_modal_file, 
     deploy_modal_app,
-    remote_deploy_modal_app,
-    remote_validate_modal_file
 )
+import modal
 
 class ValidateModalFileProvider:
     def __init__(self, settings: Settings = Depends(get_settings)):
         self.settings = settings
+        if settings.MODAL_USE_LOCAL:
+            self.f = validate_modal_file
+        else:
+            remote_function = modal.Function.lookup(
+                "garden-publishing-helpers", 
+                "remote_validate_modal_file"
+            )
+            self.f = remote_function.remote
+
 
     def __call__(self, file_contents: str):
-        f = validate_modal_file if self.settings.MODAL_USE_LOCAL else remote_validate_modal_file.remote
-        return f(file_contents)
+        return self.f(file_contents)
 
 class DeployModalAppProvider:
     def __init__(self, settings: Settings = Depends(get_settings)):
         self.settings = settings
+        if settings.MODAL_USE_LOCAL:
+            self.f = deploy_modal_app
+        else:
+            remote_function = modal.Function.lookup(
+                "garden-publishing-helpers", 
+                "remote_deploy_modal_app"
+            )
+            self.f = remote_function.remote
+
 
     def __call__(self, file_contents: str, app_name: str, token_id: str, token_secret: str, env: str):
-        f = deploy_modal_app if self.settings.MODAL_USE_LOCAL else remote_deploy_modal_app.remote
-        return deploy_modal_app(file_contents, app_name, token_id, token_secret, env)
+        return self.f(file_contents, app_name, token_id, token_secret, env)
+
