ad0990797dde07afd4ea9d9d2dd73f58cdb819db
Hayden Holbrook <111379706+hholb@users.noreply.github.com>
2024-10-16 13:15:19 -0400

    Remove search index sync (#183)
    * remove search index sync stuff
    
    * create migration script to remove failed_search_index_updates table

--- garden-backend-service/src/models/__init__.py
+++ garden-backend-service/src/models/__init__.py
@@ -1,6 +1,5 @@
 from .base import Base  # noqa
 from .entrypoint import Entrypoint  # noqa
-from .failed_search_index import FailedSearchIndexUpdate  # noqa
 from .garden import Garden  # noqa
 from .mdf.dataset import Dataset  # noqa
 from .modal.modal_app import ModalApp  # noqa
