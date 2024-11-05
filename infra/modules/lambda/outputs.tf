f6b8ab833e5045c3ff36845374b5c8b8949bae2f
Will Engler <Engler.Will@gmail.com>
2024-11-05 14:04:49 -0600

    fix copypasta Owen found in review

--- infra/modules/lambda/outputs.tf
+++ infra/modules/lambda/outputs.tf
@@ -1,19 +1,10 @@
-output "garden_app_invoke_arn" {
+output "garden_sandbox_fn_invoke_arn" {
   value = aws_lambda_function.sandboxed_app.invoke_arn
 }
 
-output "garden_app_function_name" {
+output "garden_sandbox_fn_function_name" {
   value = aws_lambda_function.sandboxed_app.function_name
 }
-
-output "garden_authorizer_invoke_arn" {
-  value = aws_lambda_function.sandboxed_app.invoke_arn
-}
-
-output "garden_authorizer_function_name" {
-  value = aws_lambda_function.sandboxed_app.function_name
-}
-
 
 output "lambda_exec_role_name" {
   value = aws_iam_role.lambda_exec.name
