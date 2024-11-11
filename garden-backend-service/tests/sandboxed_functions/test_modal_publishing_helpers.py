2a8819e7946832cae920e44d9e81df0a841fb3f4
Will Engler <Engler.Will@gmail.com>
2024-11-11 15:44:57 -0600

    fix test file

--- garden-backend-service/tests/sandboxed_functions/test_modal_publishing_helpers.py
+++ garden-backend-service/tests/sandboxed_functions/test_modal_publishing_helpers.py
@@ -1,7 +1,7 @@
 import modal
 import pytest
 
-from src.sandboxed_functions.modal_publishing_helpers import get_function_specs
+from src.sandboxed_functions.lambda_function import get_function_specs
 
 app = modal.App()
 
