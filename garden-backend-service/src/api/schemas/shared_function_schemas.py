bb0a61f667dce13b04385a42944662785684f6a2
hholb <hholbrook@uchicago.edu>
2025-03-24 16:18:59 -0400

    add owner to function responses

--- garden-backend-service/src/api/schemas/shared_function_schemas.py
+++ garden-backend-service/src/api/schemas/shared_function_schemas.py
@@ -1,4 +1,4 @@
-from pydantic import Field
+from pydantic import AliasPath, Field
 
 from .base import BaseRelatedMetadataSchema, BaseSchema, UniqueList, Url
 
@@ -40,6 +40,7 @@ class _NotebookMetadata(BaseRelatedMetadataSchema):
 
 
 class CommonFunctionMetadata(BaseSchema):
+    owner: str = Field(alias=AliasPath("owner", "name"))
     is_archived: bool = False
 
     function_text: str
