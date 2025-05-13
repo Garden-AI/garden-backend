ec2a6be99445b6bb588ebc5d907d2afd6a8c47b2
hholb <hholbrook@uchicago.edu>
2025-05-13 17:15:35 -0400

    revert doi creators change

--- garden-backend-service/src/datacite/doi_utils.py
+++ garden-backend-service/src/datacite/doi_utils.py
@@ -97,7 +97,7 @@ async def _make_request_to_datacite(
 def _datacite_metadata_from_garden(garden: Garden) -> datacite.Doi:
     creators = [
         {"nameType": "Personal", "name": name}
-        for name in set([garden.owner.name] + garden.authors + garden.contributors)
+        for name in set(garden.authors + garden.contributors)
     ]
 
     return datacite.Doi(
