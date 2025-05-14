1d69dce1505caf9743a30cfef1b012d154ff96c7
hholb <hholbrook@uchicago.edu>
2025-05-14 13:18:38 -0400

    fix tests

--- garden-backend-service/tests/api/routes/test_benchmarks.py
+++ garden-backend-service/tests/api/routes/test_benchmarks.py
@@ -4,55 +4,23 @@ import pytest
 from modal_proto import api_pb2
 
 from src.api.schemas.benchmark import (
-    BenchmarkCreateRequest,
     BenchmarkRequest,
     BenchmarkResult,
 )
 from src.modal.utils import AsyncModalJobStatus
+from src.models.benchmark import Benchmark, BenchmarkTask
 
 
-@pytest.mark.integration
-@pytest.mark.asyncio
-async def test_create_benchmark(
-    client,
-    mock_db_session,
-    override_authenticated_dependency,
-    mock_modal_publisher_auth_state,
-    mock_modal_app_create_request_one_function,
-    override_sandboxed_functions,
-):
-    # Create a modal app with at least 1 function
-    modal_app_response = await client.post(
-        "/modal-apps", json=mock_modal_app_create_request_one_function
-    )
-    assert modal_app_response.status_code == 200
-    modal_app = modal_app_response.json()
-    function_id = modal_app["modal_function_ids"][0]
-
-    # Create the benchmark
-    create_request = BenchmarkCreateRequest(function_id=function_id)
-    benchmark_response = await client.post(
-        "/benchmarks/create", json=create_request.model_dump()
-    )
-    assert benchmark_response.status_code == 201
-
-    # We should get back the metadata for the function acting as the benchmark
-    benchmark = benchmark_response.json()
-    assert benchmark == modal_app["modal_functions"][0]
-
-    duplicate_create_response = await client.post(
-        "/benchmarks/create", json=create_request.model_dump()
-    )
-    assert (
-        duplicate_create_response.status_code == 409
-    ), "Creating a benchmark that already exists should fail"
-
-    # Check that we can pull the benchmark's metadata
-    metadata_response = await client.get("/benchmarks")
-    assert metadata_response.status_code == 200
-    benchmark_meta = metadata_response.json()
-    assert len(benchmark_meta) == 1, "There should one benchmark"
-    assert benchmark_meta[0] == benchmark, "It should be the benchmark we created"
+async def create_benchmark_and_task(db_session_maker, function_id) -> (int, int):
+    benchmark = Benchmark(id=1, name="Test Benchmark")
+    task = BenchmarkTask(benchmark_id=benchmark.id, function_id=function_id)
+    async with db_session_maker() as db:
+        db.add(benchmark)
+        db.add(task)
+        await db.commit()
+        await db.refresh(benchmark)
+        await db.refresh(task)
+        return benchmark.id, task.id
 
 
 @pytest.mark.integration
@@ -60,6 +28,7 @@ async def test_create_benchmark(
 async def test_run_benchmark(
     client,
     mock_db_session,
+    async_db_session,
     override_authenticated_dependency,
     mock_modal_publisher_auth_state,
     mock_modal_app_create_request_one_function,
@@ -100,28 +69,21 @@ async def test_run_benchmark(
     )
     assert modal_app_response.status_code == 200
     modal_app = modal_app_response.json()
-    benchmark_id = modal_app["modal_function_ids"][0]
     function_id = modal_app["modal_function_ids"][0]
 
     # Create the benchmark
-    create_request = BenchmarkCreateRequest(function_id=function_id)
-    benchmark_response = await client.post(
-        "/benchmarks/create", json=create_request.model_dump()
+    benchmark_id, task_id = await create_benchmark_and_task(
+        async_db_session, function_id
     )
-    assert benchmark_response.status_code == 201
-
     # Run the benchmark
-    run_request = BenchmarkRequest(
-        function_id=function_id,
-        task_id=0,
-    )
+    run_request = BenchmarkRequest(function_id=function_id)
     run_response = await client.post(
-        f"/benchmarks/{benchmark_id}", json=run_request.model_dump()
+        f"/benchmarks/{benchmark_id}/{task_id}", json=run_request.model_dump()
     )
     assert run_response.status_code == 200
 
     # Check the benchmark results
-    results_response = await client.get(f"/benchmarks/{benchmark_id}")
+    results_response = await client.get(f"/benchmarks/{benchmark_id}/{task_id}")
     assert results_response.status_code == 200
     results = results_response.json()
     assert len(results) == 1
