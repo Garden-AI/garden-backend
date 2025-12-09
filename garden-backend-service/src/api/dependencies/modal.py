85562c0fc7dec84fc22329c8c3394beb04258038
Will Engler <Engler.Will@gmail.com>
2025-12-09 13:22:03 -0600

    make sure Modal clients get garbage collected

--- garden-backend-service/src/api/dependencies/modal.py
+++ garden-backend-service/src/api/dependencies/modal.py
@@ -1,10 +1,15 @@
+from typing import AsyncGenerator
+
 from fastapi import Depends
 
 import modal
 from src.config import Settings, get_settings
 
 
-async def get_modal_client(settings: Settings = Depends(get_settings)) -> modal.Client:
-    return await modal.client._Client.from_credentials(
+async def get_modal_client(
+    settings: Settings = Depends(get_settings),
+) -> AsyncGenerator[modal.Client, None]:
+    async with modal.client._Client.from_credentials(
         settings.MODAL_TOKEN_ID, settings.MODAL_TOKEN_SECRET
-    )
+    ) as client:
+        yield client
