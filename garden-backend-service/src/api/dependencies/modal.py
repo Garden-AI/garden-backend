5f778543ac464e547bc347cf646c50b501ffc46e
Will Engler <Engler.Will@gmail.com>
2025-12-09 13:32:56 -0600

    fix double-opening of Client

--- garden-backend-service/src/api/dependencies/modal.py
+++ garden-backend-service/src/api/dependencies/modal.py
@@ -9,8 +9,12 @@ from src.config import Settings, get_settings
 async def get_modal_client(
     settings: Settings = Depends(get_settings),
 ) -> AsyncGenerator[modal.Client, None]:
+    # from_credentials returns an already-started client
     client = await modal.client._Client.from_credentials(
         settings.MODAL_TOKEN_ID, settings.MODAL_TOKEN_SECRET
     )
-    async with client:
+    try:
         yield client
+    finally:
+        # Manually close the client since we can't use 'async with' (it would double-open)
+        await client.__aexit__(None, None, None)
