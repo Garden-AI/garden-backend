57bb20d3d5144f631b1445436b05728f7811c8ba
Will Engler <Engler.Will@gmail.com>
2025-12-09 13:37:01 -0600

    Make sure Modal clients get garbage collected (#385)
    * make sure Modal clients get garbage collected
    
    * fix failing test
    
    * fix double-opening of Client

--- garden-backend-service/src/api/dependencies/modal.py
+++ garden-backend-service/src/api/dependencies/modal.py
@@ -1,10 +1,20 @@
+from typing import AsyncGenerator
+
 from fastapi import Depends
 
 import modal
 from src.config import Settings, get_settings
 
 
-async def get_modal_client(settings: Settings = Depends(get_settings)) -> modal.Client:
-    return await modal.client._Client.from_credentials(
+async def get_modal_client(
+    settings: Settings = Depends(get_settings),
+) -> AsyncGenerator[modal.Client, None]:
+    # from_credentials returns an already-started client
+    client = await modal.client._Client.from_credentials(
         settings.MODAL_TOKEN_ID, settings.MODAL_TOKEN_SECRET
     )
+    try:
+        yield client
+    finally:
+        # Manually close the client since we can't use 'async with' (it would double-open)
+        await client.__aexit__(None, None, None)
